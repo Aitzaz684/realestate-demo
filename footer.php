0e71b5818d87b0265fc743f1dec6032c25fc7316
Muhammad Aitzaz <etzazhassan684@gmail.com>
2024-06-30 16:08:30 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="#"><i class="fas fa-phone"></i><span>03204833096</span></a>
+         <a href="#"><i class="fas fa-phone"></i><span>03134847253</span></a>
+         <a href="#"><i class="fas fa-envelope"></i><span>aitzazmaher8@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>Lahore,Pakistan</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="maher aitzaz"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr.aitzaz</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
